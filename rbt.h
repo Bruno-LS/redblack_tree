334451aac5db30af2a69522a34da9e50ed4a4587
Bruno-LS <111468518+Bruno-LS@users.noreply.github.com>
2024-06-20 21:38:51 -0300

    Update rbt.h

--- rbt.h
+++ rbt.h
@@ -6,25 +6,25 @@ typedef struct TNoRB {
     struct TNoRB *dir;
 } TNoRB;
 
-void insereNo(TNoRB **arvore, TNoRB *pPai, TNoRB **pMain, int k);
+void insereNo(TNoRB **arvore, TNoRB *pPai, TNoRB **pMain, int k);//é usada para inserir um nó em uma árvore Red-Black (RB) 
 
-TNoRB *avo(TNoRB *arvore);
+TNoRB *avo(TNoRB *arvore);// Procura o avo de um determinado nó
 
-TNoRB *tio(TNoRB *arvore);
+TNoRB *tio(TNoRB *arvore);//encontrar o tio de um nó
 
-void rotacaoDireita(TNoRB **no);
+void rotacaoDireita(TNoRB **no);// rotaciona à direita a subarvore
 
-void rotacaoEsquerda(TNoRB **no);
+void rotacaoEsquerda(TNoRB **no);//rotaciona à esquerda a subarvore
 
 void consertaRB(TNoRB **arvore, TNoRB **pMain);
 
-void inOrder(TNoRB *raiz);
+void inOrder(TNoRB *raiz);// realiza uma travessia in-order (em ordem) em uma árvore rubro-negra e imprime detalhes sobre cada nó.
 
-void dRotacaoDireita(TNoRB **no);
+void dRotacaoDireita(TNoRB **no);//realiza a rotação direita dupla, que é a rotação esquerda e depois direita 
 
-void dRotacaoEsquerda(TNoRB **no);
+void dRotacaoEsquerda(TNoRB **no);//realiza a rotação esquerda dupla, que é a rotação direita e depois esquerda 
 
-void removeNo(TNoRB **arvore, int k);
+void removeNo(TNoRB **arvore, int k);//função responsavel pela remoção
 
 void nBlackIBlackFRed(TNoRB *aRemover, TNoRB **arvore);
 
@@ -32,10 +32,10 @@ void nBlackIBlackFBlack(TNoRB *aRemover, TNoRB **arvore);
 
 void nBlackIRed(TNoRB *aRemover, TNoRB **arvore);
 
-TNoRB *buscaNo(TNoRB *arvore, int k);
+TNoRB *buscaNo(TNoRB *arvore, int k);//busca o nó
 
-TNoRB *irmao(TNoRB *pai, TNoRB *noAtual);
+TNoRB *irmao(TNoRB *pai, TNoRB *noAtual);// busca e retorna o irmão de um "nó noAtual" em relação ao seu pai pai em uma árvore
 
-int filhoEsquerdo(TNoRB *arvore);
+int filhoEsquerdo(TNoRB *arvore);//determina se um nó específico é o filho esquerdo ou direito de seu pai em uma árvore
 
-TNoRB **maiorEsq(TNoRB **pMaiorEsq);
+TNoRB **maiorEsq(TNoRB **pMaiorEsq);//encontrar o nó com o maior valor na subárvore esquerda de um nó dado em uma árvore 
